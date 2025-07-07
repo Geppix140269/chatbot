071d3a3a6edd46572ae43f822ad3baa097a36473
Geppix140269 <139779768+Geppix140269@users.noreply.github.com>
2025-07-07 09:46:31 +0200

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+// pages/index.jsx
+import ChatBot from '../components/ChatBot';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <ChatBot />
+    </div>
+  );
+}
